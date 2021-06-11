fc3f1e6adf52b92ebee4e8d94f286516465774ba
Nourreddine HOUARI <nourreddine.houari@sicpa.com>
2021-06-11 22:00:40 +0800

    Initial status

---
+++ src/main/java/com/sicpa/sso/thaisso/controller/MockDMS.java
@@ -0,0 +1,20 @@
+package com.sicpa.sso.thaisso.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("dms")
+public class MockDMS {
+
+  @GetMapping
+  public ResponseEntity<String> dms(HttpServletResponse response) {
+    String headers = response.getHeaderNames().stream().reduce("", (s1,s2)->s1+ "\n" + s2);
+    return new ResponseEntity<String>(headers, HttpStatus.OK);
+  }
+}
---
+++ src/test/java/com/sicpa/sso/thaisso/ThaissoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicpa.sso.thaisso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaissoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
